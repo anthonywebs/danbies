5058cb9fcfed2048fac5efe459e6c1b7cba7daff
Anthony Kim <okim01@nuix.com>
2024-04-08 03:04:55 -0400

    draft

---
+++ tmp.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const handleSubmit = () => {
+  alert('Invalid username or password. If you need further assistance, please reach out to our support team at contact@danbies.com.');
+}
+
+const main = () => {
+
+}
+
+main();
